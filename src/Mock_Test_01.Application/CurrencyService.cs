8f6f62b34451e31a508dc67650e99fe6ac0d500f
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-05-02 12:56:15 +0200

    service class is okay for GET

---
+++ src/Mock_Test_01.Application/CurrencyService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Mock_Test_01.Application;
+using Models;
+
+public class CurrencyService : ICurrencyService
+{
+    private string _connectionString;
+
+    public CurrencyService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public SearchResultDto SearchByCountry(string countryName)
+    {
+        var result = new SearchResultDto
+        {
+            Name = countryName,
+            Currencies = new List<CurrencyDto>()
+        };
+        
+        var sql = @"SELECT c.Name, c.Rate 
+                    FROM Currency c 
+                    JOIN Currency_Country cc ON c.Id = cc.Currency_Id
+                    JOIN Country co ON co.id = cc.Country_Id
+                    WHERE co.name = @countryName";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@countryName", countryName);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            
+            if(reader.HasRows)
+                while (reader.Read())
+                {
+                    result.Currencies.Add( new CurrencyDto
+                        {
+                            Name = reader.GetString(0),
+                            Rate = reader.GetFloat(1)
+                        }
+                    );
+                }
+        }
+        return result;
+    }
+
+    public SearchResultDto SearchByCurrency(string currencyName)
+    {
+        var result = new SearchResultDto
+        {
+            Name = currencyName,
+            Countries = new List<string>()
+        };
+
+        var sql = @"SELECT co.Name FROM Country co
+                    JOIN Currency_Contry cc ON co.Id = cc.Country_Id
+                    JOIN Currency c ON c.Id = cc.Currency_Id
+                    WHERE c.Name = @currencyName";
+
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@currencyName", currencyName);
+            
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            
+            if(reader.HasRows)
+                while (reader.Read())
+                {
+                    result.Countries.Add(reader.GetString(0));
+                }
+        }
+        return result;
+    }
+}
---
+++ src/Mock_Test_01.Application/ICurrencyService.cs
@@ -0,0 +1,10 @@
+﻿using Mock_Test_01.Models;
+
+namespace Mock_Test_01.Application;
+
+public interface ICurrencyService
+{
+    // public bool InsertCurrency(CurrencyDto currencyDto);
+    public SearchResultDto SearchByCountry(string country);
+    public SearchResultDto SearchByCurrency(string currency);
+}
